1ef7a9c38108685a9656b53bafe41ae5a0b56f2d
pinkywhale <erica_eli@hotmail.com.ar>
2018-05-09 22:30:02 -0300

    commit

---
+++ ej01/ej01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero:");
+
+            String txtIngreso;
+
+            txtIngreso = Console.ReadLine();
+
+            int numIngreso = Int16.Parse(txtIngreso); // Parseo de 
+
+            int resultado;
+
+            resultado = numIngreso * 2;
+
+            Console.WriteLine("La multiplicacion x 2 del nuemro es: " + resultado);
+            Console.WriteLine("Pausa, pulse enter para terminar.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ej03/ej03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el precio del producto: ");
+
+            String precio;
+            precio = Console.ReadLine();
+
+            int numPrecio;
+            numPrecio = Int16.Parse(precio);
+
+            int iva;
+            iva = (numPrecio * 21) / 100;
+
+            int resultado_iva;
+            resultado_iva = numPrecio + iva ;
+
+            Console.WriteLine("El precio es: " + precio );
+            Console.WriteLine("El IVA es de: " + iva);
+            Console.WriteLine("El precio total es: " + resultado_iva);
+            Console.ReadLine();
+
+        
+        }
+    }
+}
